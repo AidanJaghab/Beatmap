0b9f24e007d2f1edacf5b9fa9f9adb8546f0c335
Aidan Jaghab <87317539+AidanJaghab@users.noreply.github.com>
2025-07-28 15:00:44 -0400

    Update scraper.py

--- backend/scraper.py
+++ backend/scraper.py
@@ -61,10 +61,7 @@ def scrape_edmtrain_nyc():
             today.strftime("%b %d"),
         ]
 
-        lines = [line.strip() for line in page_text.split('\n')]
-
-lines = [line.strip() for line in page_text.split('\n') if line.strip()]
-
+        lines = [line.strip() for line in page_text.split('\n') if line.strip()]
         events = []
 
         i = 0
